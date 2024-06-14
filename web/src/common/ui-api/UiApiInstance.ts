970bc84260ca7959a7403bfdae539351b657eb30
David Choi <dharric@live.com>
2024-06-14 09:56:09 -0400

    cleanup

--- web/src/common/ui-api/UiApiInstance.ts
+++ web/src/common/ui-api/UiApiInstance.ts
@@ -3,14 +3,11 @@ import { IrysCommon } from "../api/irys/IrysCommon";
 import { IrysReadApi } from "../api/irys/IrysReadApi";
 import { IrysWriteApi } from "../api/irys/IrysWriteApi";
 import { UiApi } from "./UiApi";
-import Solflare from "@solflare-wallet/sdk";
 
 let uiApi: UiApi;
 
 /// always returns same instance
-export async function initOrGetUiApi(
-  walletProvider: Solflare | null | undefined
-) {
+export async function initOrGetUiApi() {
   if (!uiApi) {
     const irysCommon = new IrysCommon();
     irysCommon.Network = NETWORK;
@@ -18,7 +15,6 @@ export async function initOrGetUiApi(
     const irysRead = new IrysReadApi(irysCommon);
 
     uiApi = new UiApi(new IrysWriteApi(irysCommon, irysRead), irysRead);
-    await uiApi.connect(walletProvider);
   }
   return uiApi;
 }
