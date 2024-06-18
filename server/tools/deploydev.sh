0f104ff779c5e31cfb3989a395e70425c0c7daf3
David Choi <dharric@live.com>
2024-06-18 08:12:30 -0400

    fix issue with deploydev.sh script

--- server/tools/deploydev.sh
+++ server/tools/deploydev.sh
@@ -2,7 +2,7 @@ db_name="postgres:14-alpine"
 api_name="freeauthapi"
 
 echo "clear docker images"
-container_ids=$(docker ps -q --filter "ancestor=$db_name" --filter "ancestor=api_name")
+container_ids=$(docker ps -q --filter "ancestor=$db_name" --filter "ancestor=$api_name")
 if [ -z "$container_ids" ]; then
   echo "No container id found using image: $db_name or image: $api_name"
 else
@@ -15,5 +15,5 @@ fi
 echo "build new images"
 docker compose -f docker-compose.dev.yml up -d --build
 
-echo "setup test data"
+# echo "setup test data"
 # psql -h localhost -p 5433 -d freeauth -U freeauth -f ./tools/setup-dev-data.sql
